7df26913b0b6592af5bc454e606c2c1725278515
Catalina Montes de Oca <catumdeoca@gmail.com>
2022-04-03 21:05:22 -0300

    counter

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './ItemCount.css'
+
+const Counter = ( {initial, stock, onAdd} ) => {
+    let [count, setCount] = useState(initial);
+
+    // useEffect(() => {
+    //     console.log('Se montó el cmponente');
+    // }, [])
+
+    const increment = () => {
+        if (count < stock) {
+            setCount(count + 1);            
+        }
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    return(
+        <div className='itemCountDiv'>
+            <button onClick={increment}>+</button>
+            <p>{count}</p>
+            <button onClick={decrement}>-</button>
+            <div className='divAgregar'>
+                <button className='buttonAgregar' onClick={() => {onAdd(count); setCount(0)}}>Agregar al carrito</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Counter
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,15 @@
 import './ItemListContainer.css';
+import Counter from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
+    const handleOnAdd = (quantity) =>{
+        console.log(`Se agregaron ${quantity} productos al carrito`)
+    }
+    
     return(
         <div className="div">
             <h2>{props.greeting}</h2>
+            <Counter  initial={0} stock={10} onAdd={handleOnAdd} />
         </div>
     )
 }
